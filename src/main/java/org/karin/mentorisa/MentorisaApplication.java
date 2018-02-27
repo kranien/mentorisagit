e0d300fd1776bca1bdd1f1afa5555fe1506de00e
karinhakansson <karin.r.hakansson@gmail.com>
2018-02-27 21:10:28 +0100

    first commit

---
+++ src/main/java/org/karin/mentorisa/MentorisaApplication.java
@@ -0,0 +1,12 @@
+package org.karin.mentorisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorisaApplication.class, args);
+	}
+}
---
+++ src/test/java/org/karin/mentorisa/MentorisaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.karin.mentorisa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MentorisaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
